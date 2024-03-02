a77fb4edd1586dde99dc0be166ac8f240f787e46
ArpitTiwari1999 <arpit.chdtiwari@gmail.com>
2024-03-02 15:59:04 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './Banner.css';
+import './Newsletter.css';
+import bannerImage from './images/banner.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="banner">
+          <img src={bannerImage} alt="Banner" />
+          <h1>Welcome to 4AM</h1>
+          <p>We're not early birds or night owls. We're some form of permanently exhausted pigeons.</p>
+        </div>
       </header>
+      <footer>
+        <div className="newsletter">
+          <h2>Join our newsletter</h2>
+          <form>
+            <input type="email" placeholder="Your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
+      </footer>
     </div>
   );
 }
